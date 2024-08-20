0e25803311ca55827ee1e66b5fa980ec533e67ea
Shlubka <kirillkostin2020@gmial.com>
2024-08-21 02:11:07 +0300

    победа, есть редирект на post

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::http::uri::Uri;
+use rocket::fs::{relative, FileServer};
+use rocket::response::Redirect;
 use rocket::serde::{json::Json, Deserialize};
-use rocket::{fs::NamedFile, response::Redirect};
-use std::path::{Path, PathBuf};
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -14,20 +13,8 @@ struct UserData<'r> {
     promo: Option<&'r str>,
 }
 
-#[get("/")]
-fn index() -> Redirect {
-    let redirect = Redirect::to(uri!("/index"));
-    redirect
-}
-
-#[get("/index")]
-async fn home() -> Option<NamedFile> {
-    NamedFile::open("index.html").await.ok()
-}
-
 #[post("/lang", data = "<data>")]
 fn get_keys(data: Json<UserData<'_>>) -> Redirect {
-    // Log the received data
     rocket::info!(
         "Received data: language = {}, code = {}, promo = {:?}",
         data.language,
@@ -35,37 +22,28 @@ fn get_keys(data: Json<UserData<'_>>) -> Redirect {
         data.promo
     );
 
-    // Extract fields from the JSON payload
     let language = data.language.to_string();
     let code = data.code.to_string();
     let promo = data.promo.map(|s| s.to_string()).unwrap_or_default();
 
-    // Construct the redirect URI
     let local_link = format!("/lang/{}?code={}&promo={}", language, code, promo);
     rocket::info!("Redirect to {local_link}");
 
-    // Redirect to the constructed URI
-    Redirect::to(uri!(start_logick(language, code, promo)))
+    Redirect::to(uri!(start_logick(language)))
 }
 
-#[get("/lang/<lang>?<code>&<promo>")]
-fn start_logick(lang: &str, code: Option<&str>, promo: Option<&str>) -> String {
-    // Return a formatted string containing the query parameters
-    let code = code.unwrap_or("");
-    let promo = promo.unwrap_or("");
+#[get("/lang/<lang>")]
+fn start_logick(lang: &str) -> String {
     rocket::info!("Enter in logika",);
     format!(
         "Lang = \"{}\"; \nCode = \"{}\" \nPromoCode = \"{}\"",
-        lang, code, promo
+        lang, "code", "promo"
     )
-}
-
-#[get("/<file..>")]
-async fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("").join(file)).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, home, get_keys, files, start_logick])
+    rocket::build()
+        .mount("/", routes![/*index, home,, files*/ get_keys, start_logick])
+        .mount("/", FileServer::from(relative!("static")))
 }
---
+++ static/js/json1.js
@@ -0,0 +1,71 @@
+let selectedOption = "";
+
+function toggleDropdown() {
+  document.getElementById("dropdownContent").classList.toggle("show");
+}
+
+function selectOption(value, text) {
+  selectedOption = value;
+  document.getElementById("dropdownButton").textContent = text;
+  document.getElementById("dropdownContent").classList.remove("show");
+}
+
+function showLanguageAlert(message) {
+  alert(message);
+  document.getElementById("dropdownContent").classList.remove("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches("#dropdownButton")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function preventFormSubmit(event) {
+  event.preventDefault();
+}
+
+async function submitForm() {
+  const textInput = document.getElementById("textInput").value.trim();
+
+  if (textInput === "" || selectedOption === "") {
+    document.getElementById("textInput").style.borderColor =
+      textInput === "" ? "red" : "#ccc";
+    document.getElementById("dropdownButton").style.borderColor =
+      selectedOption === "" ? "red" : "#ccc";
+
+    setTimeout(() => {
+      document.getElementById("textInput").style.borderColor = "#ccc";
+      document.getElementById("dropdownButton").style.borderColor = "#ccc";
+    }, 2500);
+
+    return;
+  }
+
+  const url = `http://127.0.0.1:8000/lang`;
+  //const url = `https://buzzard-grateful-wren.ngrok-free.app/lang/`;
+
+  const data = {
+    language: selectedOption,
+    code: textInput,
+    promo: "none",
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.redirected) {
+      window.location.href = response.url;
+    }
+  });
+}
